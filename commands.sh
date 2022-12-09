2f833201e26d6710c13cf78f2ed9e9e83b1074db
Michal Marhan <marhanmichal@gmail.com>
2022-12-09 22:58:14 +0100

    Build and run Docker eshop

--- commands.sh
+++ commands.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 yum install git && \
-pip3 install ansible boto boto3
+pip3 install ansible boto boto3 && \
+git clone https://github.com/MarhyCZ/4IT572_ansible.git && \
+ansible-vault create ~/4IT572_ansible/ansible/group_vars/all/aws.yml
