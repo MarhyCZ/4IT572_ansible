828eb9fd67e3eb07062cc7127b96627168888bba
Michal Marhan <marhanmichal@gmail.com>
2024-01-27 19:20:33 +0100

    Update commands.sh

--- commands.sh
+++ commands.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-yum install git && \
+apt install git && \
 pip3 install ansible boto boto3 && \
 git clone https://github.com/MarhyCZ/4IT572_ansible.git && \
 ansible-vault create ~/4IT572_ansible/ansible/group_vars/all/aws.yml
